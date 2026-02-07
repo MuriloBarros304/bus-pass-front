a75123a0d14cac2b1e4b038ae73fd7baf9e0286f
Murilo <murilobarros304@gmail.com>
2026-02-07 14:49:50 -0300

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+
+function Home() {
+    return (
+        <div>
+                <h1>Testando</h1>
+                <p>Página inicial</p>
+        </div>
+    );
+}
+
+export default Home;
